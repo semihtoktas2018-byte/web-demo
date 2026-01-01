6bcd22343b77d15bc00608c80d7f27897c3400be
semihtoktas2018-byte <semihtoktas2018@gmail.com>
2026-01-02 00:09:50 +0300

    ilk yayin

---
+++ Musa Budan/assets/app.js
@@ -0,0 +1,14 @@
+new TradingView.widget({
+  autosize: true,
+  symbol: "BIST:THYAO",
+  interval: "15",
+  timezone: "Europe/Istanbul",
+  theme: "light",
+  style: "1",
+  locale: "tr",
+  enable_publishing: false,
+  hide_top_toolbar: false,
+  hide_legend: false,
+  save_image: false,
+  container_id: "tv_aksen"
+});
